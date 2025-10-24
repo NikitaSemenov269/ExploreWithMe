d7ecd94e2d3f490dfb0da6f63d287003649fb94b
vil-shavetail <demid.ivan@gmail.com>
2025-10-24 14:02:45 +0300

    Добавление HttpClient в сервисе статистики.

---
+++ stats/stats-client/src/main/java/ru/practicum/client/StatsController.java
@@ -0,0 +1,43 @@
+package ru.practicum.client;
+
+import jakarta.validation.Valid;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import ru.practicum.DTO.RequestStatisticDto;
+import ru.practicum.DTO.ResponseStatisticDto;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+
+@RestController
+public class StatsController {
+    private final StatsClient statsClient;
+
+    public StatsController(StatsClient statsClient) {
+        this.statsClient = statsClient;
+    }
+
+    @PostMapping("/hit")
+    public ResponseEntity<Void> saveHit(@Valid @RequestBody RequestStatisticDto requestStatisticDto) {
+        statsClient.saveHit(requestStatisticDto);
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/stats")
+    public ResponseEntity<ResponseStatisticDto> getStats(
+            @RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") LocalDateTime start,
+            @RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") LocalDateTime end,
+            @RequestParam(required = false) List<String> uris,
+            @RequestParam(defaultValue = "false") boolean unique
+    ) {
+        return ResponseEntity.ok(statsClient.getStats(
+                start.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")),
+                end.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")),
+                uris,
+                unique
+        ));
+    }
+
+}
