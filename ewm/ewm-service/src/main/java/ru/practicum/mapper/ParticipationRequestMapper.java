1857557ec4cdeebe74d45fcd2adccfc5af8388d5
vil-shavetail <demid.ivan@gmail.com>
2025-11-04 17:26:02 +0300

    Исправление checkStyle errors.

--- ewm/ewm-service/src/main/java/ru/practicum/mapper/ParticipationRequestMapper.java
+++ ewm/ewm-service/src/main/java/ru/practicum/mapper/ParticipationRequestMapper.java
@@ -9,6 +9,7 @@ import ru.practicum.model.ParticipationRequest;
 @Mapper(componentModel = "spring")
 public interface ParticipationRequestMapper {
     ParticipationRequestMapper INSTANCE = Mappers.getMapper(ParticipationRequestMapper.class);
+
     @Mapping(target = "event", source = "event.id")
     @Mapping(target = "requester", source = "requester.id")
     @Mapping(target = "created", source = "created", dateFormat = "yyyy-MM-dd HH:mm:ss")
