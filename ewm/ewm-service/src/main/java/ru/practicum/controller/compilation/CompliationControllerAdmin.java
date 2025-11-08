2272e265ff587df08f120bd8cf71df5b1dab7677
vil-shavetail <demid.ivan@gmail.com>
2025-11-08 15:26:56 +0300

    Доработан endpoint обновления подборок.

--- ewm/ewm-service/src/main/java/ru/practicum/controller/compilation/CompliationControllerAdmin.java
+++ ewm/ewm-service/src/main/java/ru/practicum/controller/compilation/CompliationControllerAdmin.java
@@ -3,14 +3,16 @@ package ru.practicum.controller.compilation;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Min;
 import lombok.RequiredArgsConstructor;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ru.practicum.dto.compilation.CompilationDto;
 import ru.practicum.dto.compilation.NewCompilationDto;
 import ru.practicum.dto.compilation.UpdateCompilationRequest;
+import ru.practicum.exception.BadRequestException;
+import ru.practicum.exception.NotFoundException;
 import ru.practicum.service.CompilationService;
-
 
 @RestController
 @RequiredArgsConstructor
@@ -33,8 +35,31 @@ public class CompliationControllerAdmin {
     @PatchMapping("/{compId}")
     public ResponseEntity<CompilationDto> updateCompilation(
             @PathVariable @Min(1) Long compId,
-            @RequestBody UpdateCompilationRequest updRequestCompilationDto) {
-        return ResponseEntity.ok().body(compServ.updateCompilation(compId, updRequestCompilationDto));
+            @RequestBody UpdateCompilationRequest updRequestCompilationDto
+    ) {
+        try {
+            CompilationDto result = compServ.updateCompilation(compId, updRequestCompilationDto);
+            return ResponseEntity.ok().body(result);
+
+        } catch (DataIntegrityViolationException e) {
+            String errorMsg = e.getMessage();
+
+            // Проверка на превышение длины поля
+            if (errorMsg != null && errorMsg.contains("value too long")) {
+                throw new BadRequestException(
+                        "Длина поля 'title' превышает допустимый лимит (50 символов). " +
+                                "Переданное значение: '" + updRequestCompilationDto.getTitle() + "'"
+                );
+            }
+
+            // Все остальные случаи передаём глобальному обработчику
+            throw e;
+        }
+        // Обработка NotFoundException
+        catch (NotFoundException e) {
+            return ResponseEntity.notFound().build();
+        }
     }
+
 }
 
