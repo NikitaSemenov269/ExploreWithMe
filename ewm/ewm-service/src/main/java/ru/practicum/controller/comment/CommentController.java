3be4dbc46b0b4a3b400ae820e48b009a588447c7
vil-shavetail <demid.ivan@gmail.com>
2025-11-13 15:33:19 +0300

    Добавил недостаующие эндпоинты.

--- ewm/ewm-service/src/main/java/ru/practicum/controller/comment/CommentController.java
+++ ewm/ewm-service/src/main/java/ru/practicum/controller/comment/CommentController.java
@@ -1,15 +1,29 @@
 package ru.practicum.controller.comment;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import ru.practicum.dto.comment.CommentDto;
 import ru.practicum.service.CommentService;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping()
+@RequestMapping("/users/{userId}/events/{eventId}/comments")
 public class CommentController {
     private final CommentService commService;
 
+    @GetMapping
+    public ResponseEntity<List<CommentDto>> getEventComments(
+            @PathVariable @NotNull Long eventId) {
+
+        List<CommentDto> dtos = commService.findAllByIdEvent(eventId);
+        return ResponseEntity.ok(dtos);
+    }
 
 }
--- ewm/ewm-service/src/main/java/ru/practicum/controller/comment/CommentControllerPrivate.java
+++ ewm/ewm-service/src/main/java/ru/practicum/controller/comment/CommentControllerPrivate.java
@@ -2,30 +2,37 @@ package ru.practicum.controller.comment;
 
 import jakarta.validation.constraints.Min;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import ru.practicum.dto.comment.CommentDto;
 import ru.practicum.dto.comment.NewCommentDto;
 import ru.practicum.service.CommentService;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping()
+@RequestMapping("/users/{userId}/comments")
 public class CommentControllerPrivate {
+    /*
+     * добавление комментариев;
+     * удаление комментариев;
+     *  */
     private final CommentService commService;
 
+    @PostMapping
     public ResponseEntity<CommentDto> addComment(
             @RequestParam @Min(1) Long userId,
             @RequestBody NewCommentDto newCommentDto) {
 
         return ResponseEntity.ok(commService.addComment(userId, newCommentDto));
-    /*
-     * добавление комментариев;
-     * удаление комментариев;
-     *  */
+    }
+
+    @DeleteMapping("/{commentId}")
+    public ResponseEntity<Void> deleteComment(
+            @RequestParam @Min(1) Long userId,
+            @PathVariable @Min(1) Long commentId) {
+
+        commService.deleteComment(userId, commentId);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
 }
