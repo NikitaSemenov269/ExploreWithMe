67c1ce8088bc3502918228c0c7bb4681a78675a8
Nikita Semenov <nik.269@yandex.ru>
2025-11-14 12:31:00 +0300

    Минорные правки.

--- ewm/ewm-service/src/main/java/ru/practicum/service/CommentService.java
+++ ewm/ewm-service/src/main/java/ru/practicum/service/CommentService.java
@@ -51,10 +51,6 @@ public class CommentService {
             throw new ConflictException("Статус события: " + event.getState() + " не соответствует ожидаемому.");
         }
 
-      /*  if (LocalDateTime.now().isBefore(event.getEventDate())) {
-            throw new ConflictException("Возможность прокомментировать событие откроется после его окончания.");
-        }*/
-
         /*
          * Нужна ли пре-модерация заявок на участие
          * true - заявки должны одобряться инициатором
